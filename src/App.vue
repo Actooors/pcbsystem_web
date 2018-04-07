19a1a2d9b259edad7836a6250a7d231617d01241
mzz2017 <m@mzz.pub>
2018-04-07 23:29:56 +0800

    切换到饿了么font-family

--- src/App.vue
+++ src/App.vue
@@ -42,11 +42,9 @@
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    display: flex;
-    display: -webkit-flex;
   }
 
   .btn-sidebar {
