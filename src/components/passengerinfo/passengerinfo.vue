05135fa89dbde1f393052a1a4222f56cd82b61a3
ooobsidian <obsidian_lrx@aliyun.com>
2018-04-08 20:53:58 +0800

    添加了阴影

--- src/components/passengerinfo/passengerinfo.vue
+++ src/components/passengerinfo/passengerinfo.vue
@@ -2,6 +2,21 @@
   <div class="passengerinfo">
     <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
       <el-tab-pane label="我的资料" name="first">
+        <div class="UserImg">
+          <span class="userimg">头像</span>
+          <div class="img">
+            <el-upload
+              action="https://jsonplaceholder.typicode.com/posts/"
+              list-type="picture-card"
+              :on-preview="handlePictureCardPreview"
+              :on-remove="handleRemove">
+              <i class="el-icon-plus"></i>
+            </el-upload>
+            <el-dialog :visible.sync="dialogVisible">
+              <img width="100%" :src="dialogImageUrl" alt="">
+            </el-dialog>
+          </div>
+        </div>
         <div class="UserId">
           <span class="userid">用户ID</span>
           <div class="id">
@@ -33,16 +48,6 @@
             </el-input>
           </div>
         </div>
-        <div class="UserImg">
-          <span class="userimg">头像地址</span>
-          <div class="Img">
-            <el-input
-              placeholder="http://alphalrx.cn/images/avatar.png"
-              v-model="input1"
-              :disabled="true">
-            </el-input>
-          </div>
-        </div>
       </el-tab-pane>
       <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
       <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
@@ -57,12 +62,21 @@
       return {
         activeName2: 'first',
         input1: '',
-        input10: ''
+        input10: '',
+        dialogImageUrl: '',
+        dialogVisible: false
       };
     },
     methods: {
       handleClick(tab, event) {
         console.log(tab, event);
+      },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePictureCardPreview(file) {
+        this.dialogImageUrl = file.url;
+        this.dialogVisible = true;
       }
     }
   }
