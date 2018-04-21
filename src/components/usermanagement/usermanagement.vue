58fcbc85a1d7b36f589060c5fabaee9bc6e6adde
mzz2017 <m@mzz.pub>
2018-04-21 18:08:45 +0800

    fix:booking界面必須選擇now之後的時間點

--- src/components/usermanagement/usermanagement.vue
+++ src/components/usermanagement/usermanagement.vue
@@ -2,32 +2,43 @@
   <div class="page-wrapper">
     <el-tabs type="border-card" class="tabs">
       <el-tab-pane label="乘客用户">
+        <div class="search">
+          <el-input placeholder="搜索..." v-model="searchInput" class="input"></el-input>
 
+        </div>
       </el-tab-pane>
       <el-tab-pane label="司机用户">
-
-      </el-tab-pane>
-      <el-tab-pane label="管理员用户">
-
+        <div class="search">
+          <el-input placeholder="搜索..." v-model="searchInput" class="input"></el-input>
+        </div>
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 <script>
-
+  import axios from 'axios'
 
   export default {
     name: "usermanagement",
     components: {},
+    data() {
+      return {
+        searchInput: ''
+      }
+    },
+    mounted() {
+      console.log('mounted!')
+      axios.get('/api/retrieval')
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   @import '../../common/css/usermanagement';
-</style>
-
-<style lang="scss">
-  .tabs{
-
-  }
 </style>
