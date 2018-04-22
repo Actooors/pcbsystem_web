b6339e5bf7b78eda5a193b799815cc56195d6746
mzz2017 <m@mzz.pub>
2018-04-23 02:07:07 +0800

    放弃infoview作为base组件，放入普通组件继续开发，继续完善用户管理

--- src/components/usermanagement/usermanagement.vue
+++ src/components/usermanagement/usermanagement.vue
@@ -2,24 +2,42 @@
   <div class="page-wrapper">
     <el-tabs type="border-card" class="tabs">
       <el-tab-pane label="乘客用户">
-        <info-view :items="items['passenger']"
-                   :item-map="itemMap['passenger']"></info-view>
+        <info-view :items="items['passenger']" :item-map="itemMap['passenger']"
+                   button-title="记录查询" @on-button-click=""></info-view>
       </el-tab-pane>
       <el-tab-pane label="司机用户">
-        <info-view :items="items['driver']" :item-map="itemMap['driver']"
-                   ref="driver"></info-view>
+        <info-view :items="items['driver']"
+                   :item-map="itemMap['driver']"
+                   :color-map="colorMap"
+                   button-title="记录查询" @on-button-click="handleOnLogButtonClickDriver"
+                   button-title2="操作" @on-button-click2="handleOnOperationButtonClickDriver"></info-view>
       </el-tab-pane>
     </el-tabs>
+    <div class="logs-dialog-wrapper" v-if="showLogs">
+      <x-dialog v-model="showLogs" class="logs-dialog" hide-on-blur>
+        <p class="logs-dialog-title">预约记录</p>
+        <div class="content-box">
+          <img src="https://ws1.sinaimg.cn/large/663d3650gy1fq6824ur1dj20ia0pydlm.jpg" style="width: 100%;">
+        </div>
+        <div @click="showLogs=false">
+          <span class="vux-close"></span>
+        </div>
+      </x-dialog>
+    </div>
+
+
   </div>
 </template>
 <script>
   import axios from 'axios'
-  import InfoView from 'base/infoview/infoview'
+  import InfoView from 'components/infoview/infoview'
+  import {XDialog} from 'vux'
 
   export default {
     name: "usermanagement",
     components: {
-      InfoView
+      InfoView,
+      XDialog
     },
     data() {
       return {
@@ -43,6 +61,7 @@
             model: '奥迪A8',
             capacity: 4,
             cid: 32,
+            ustate: '正常'
           },]
         },
         items: {
@@ -63,7 +82,15 @@
             capacity: '载客数'
           }
         },
-        inputTimer: undefined
+        inputTimer: undefined,
+        showLogs: false,
+        showOperationMenu: false,
+        operationUsername: '',
+        operationMenuDriver: ['冻结用户', '删除用户'],
+        colorMap: {
+          '冻结': '#8cc5ff',
+          '报修': '#A0DB94'
+        }
       }
     },
     mounted() {
@@ -76,9 +103,32 @@
           console.log(e)
         })
     },
+    methods: {
+      handleOnLogButtonClickDriver(index) {
+        this.showLogs = true
+
+      },
+      handleOnOperationButtonClickDriver(index) {
+        this.showOperationMenu = true
+        console.log(index, this.items['driver'])
+        this.operationUsername = this.items['driver'][index].uname
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   @import '../../common/css/usermanagement';
+</style>
+<style lang="less" scoped>
+  @import '~vux/src/styles/close';
+</style>
+
+<style lang="scss">
+  .logs-dialog {
+    .weui-dialog {
+      max-width: none;
+    }
+  }
+
 </style>
